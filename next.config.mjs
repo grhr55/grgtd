6c56b8077d96c5e2ecc1897795ec66bf0c652b0e
kali <kali@kali>
2026-02-14 01:17:55 +0200

    Обновление проекта: все изменения

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "peaceful-youtiao-a888bf.netlify.app",
+      },
+    ],
   },
-}
+};
 
 export default nextConfig;
